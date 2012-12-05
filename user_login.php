17c1167373bdbd514c5dc89ed8a121d2666a274d
Kameron Weaver <kaweaver@mines.edu>
2012-12-04 19:26:57 -0700

    fixed the user login page to hopefully make it check for users and the correct password

--- user_login.php
+++ user_login.php
@@ -6,7 +6,23 @@
 	}
 	else{
 		//TODO: check username in the database
-		$_SESSION['user']=$_POST['username'];
-		header("Location: profile.php");
+		$username = strip_tags($_POST['username']);
+		$password = strip_tags($_POST['password']);
+		@ $db = new mysqli(localhost, root, '', team06);
+		$result = $db->query("SELECT * FROM USERS WHERE username = '{$username}' AND PASSWORD = '{$password}'");
+		$count = mysqli_num_rows($result);
+
+		if ($count != 0) {
+			$user = $result->fetch_assoc();
+			$_SESSION['logged_on'] = true;
+
+			header("Location: home.php");
+		}
+		else {
+			$_SESSION['logged_on'] = false;
+			$_SESSION['failure'] = "Failed to log in";
+			header("Location: incorrect_user.php");	
+		}
+		$db->close();
 	}
 ?>
