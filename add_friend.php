55b4de5552118ed9df3b18617f77cf7e26940fbf
Travis Bybee <travis.bybee@gmail.com>
2012-12-05 04:09:47 -0700

    Added a user to the FRIENDS table works correctly.

--- add_friend.php
+++ add_friend.php
@@ -4,25 +4,27 @@
 
 	@ $db = new mysqli(localhost, root, '', team06);
 	
-	$result = $db->query("SELECT * FROM USERS WHERE username = $_SESSION['user']");
-	$temp = $results->fetch_array();
-	$userid = $temp['id'];
-	var_dump($userid);
-	/*
-	$result = $db->query("SELECT * FROM USERS WHERE username = $_POST['new_friend_username']");
-	$temp = $results->fetch_array();
-	$friendid = $temp['id'];
+	$result = $db->query("SELECT id FROM USERS WHERE username = '".$_SESSION['user']."';");
+	$userid = $result->fetch_array(MYSQLI_ASSOC);
+
+	$result = $db->query("SELECT * FROM USERS WHERE username = '".$_POST['new_friend_username']."';");
+	$friendid = $result->fetch_array(MYSQLI_ASSOC);
 	
-	$result = $db->query("INSERT INTO FRIENDS (userid, friendid) VALUES ('{$suerid}', '{$friendid}');");
-	
+	$result = $db->query("SELECT * FROM FRIENDS WHERE userid = '".$userid['id']."' and friendid = '".$friendid['id']."';");
+	if( $result->num_rows == 1 ){
+?>
+		<p><?= $_POST['new_friend_username']; ?> is already your friend.</p>
+<?php
+	}
+	else{
+		$result = $db->query("INSERT INTO FRIENDS (userid, friendid) VALUES ('{$userid['id']}', '{$friendid['id']}');");
+?>
+		<h2>Add a Friend</h2>
+		<p><?= $_POST['new_friend_username']; ?> added as a friend.</p>
+	</body>
+</html>
+<?php
+	}
 	$result->free();
 	$db->close();
-	*/
-	
 ?>
-
-<h2>Add a Friend</h2>
-<p><?= $_POST['new_friend_username']; ?> added as a friend.</p>
-
-</body>
-</html>
--- friend_list.php
+++ friend_list.php
@@ -2,7 +2,32 @@
 	require('header.php');
 	
 	$db = new mysqli(localhost, root, '', team06);
-	$result = $db->query("SELECT * FROM USERS");
+	$result = $db->query("SELECT id FROM USERS WHERE username = '".$_SESSION['user']."';");
+	$userid = $result->fetch_array(MYSQLI_ASSOC);
+	$result = $db->query("SELECT friendid FROM FRIENDS WHERE userid = '".$userid['id']."';");
+	
 ?>
-
-<h2>Friend List</h2>
+	<h2>Friend List</h2>
+	<table cellpadding="5">
+		<tr>
+		<th>Username</th>
+		<th></th>
+		</tr>
+		<?php
+		while( $row = $result->fetch_array(MYSQLI_ASSOC)){
+			$newResult = $db->query("SELECT * FROM USERS WHERE id = '".$row['friendid']."';");
+			$friend = $newResult->fetch_array(MYSQLI_ASSOC);
+?>
+			<tr>
+			<td><?php echo $friend['username']; ?></td>
+			<td><input type="submit" value="No functionality here!"/></td>
+			</tr>
+<?php
+		}
+?>
+		</table>
+<?php
+	
+	$result->free();
+	$db->close();
+?>
