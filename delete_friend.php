87fcaef9bc557fa3798c958d5a6bef75e73e3f96
Travis Bybee <travis.bybee@gmail.com>
2012-12-07 17:33:12 -0700

    Added undo and return to friend list functionality in delete_friend.php.

--- delete_friend.php
+++ delete_friend.php
@@ -13,6 +13,13 @@
 
 	<h2>Remove Friend</h2>
 		<p><?= $_POST['delete_friend_email']; ?> removed from friend list.</p>
+		<form method="post" action="add_friend.php">
+			<input name="new_friend_email" value="<?= $_POST['delete_friend_email']; ?>" type="hidden"/>
+			<input type="submit" value="Undo"/>
+		</form>
+		<form method="get" action="friend_list.php">
+			<input type="submit" value="Okay"/>
+		</form>
 	</body>
 </html>
 
