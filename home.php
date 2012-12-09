4c6ca9f3738280ed52e992a24be8f0a8606a8108
Travis Bybee <travis.bybee@gmail.com>
2012-12-09 08:18:25 -0700

    Home now shows 20 status updates from the user and their friends. Copied the show status code from profile to home. Changed the for loop to stop after 20 updates or after there are no more updates.

--- home.php
+++ home.php
@@ -1,10 +1,38 @@
 <?php
 	require('header.php');	
+	require('database.php');
 ?>
 
 	<div>
 		<p><?php echo $_SESSION['email']; ?>, you logged in!</p>
 	</div>
+	<h2>Status Updates</h2>
+	<?php
+			$result = $db->query("SELECT id FROM USERS WHERE email = '".$_SESSION['email']."';");
+			$userid = $result->fetch_array(MYSQLI_ASSOC);
+			$userNum=$userid['id'];
+			$query_string="SELECT DISTINCT STATUS_UPDATES.* FROM FRIENDS, STATUS_UPDATES WHERE FRIENDS.userid =  '$userNum' AND STATUS_UPDATES.userid = FRIENDS.friendid OR STATUS_UPDATES.userid = '$userNum'";
+			$friends=$db->query($query_string);
+			$rows=$friends->num_rows;
+			for($i=0; $i<20; $i++){
+				$currentRow=$friends->fetch_assoc();
+				if($currentRow == NULL){
+					break;
+				}
+				
+				$currentUserNum=$currentRow['userid'];
+				$userDbData=$db->query("SELECT * FROM USERS WHERE id = '$currentUserNum'");
+				$userData=$userDbData->fetch_assoc();
+				
+				$userFirstName=$userData['firstname'];
+				$userLastName=$userData['lastname'];
+				$updateTime = $currentRow['lastUpdated'];
+				$status=$currentRow['status'];
+
+				echo "<h4>At $updateTime $userFirstName $userLastName said:</h4>";
+				echo "<p>$status</p>";
+			}
+		?>
 
 </body>
 </html>
